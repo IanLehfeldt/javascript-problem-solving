2b208c07e0f05d6da53d2d59db473c836fb28449
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-12-11 12:10:03 -0600

    added in jquery to handle events

--- public/client.js
+++ public/client.js
@@ -1,41 +1,45 @@
-console.log('Javascript sourced');
+// console.log('Javascript sourced');
 
-LongestWord = (sen) => {
-    var longWord = "";
-    var splitSen = sen.split(" ");
-
-    for (var i = 0; i < splitSen.length; i++) {
-        if (longWord.length < splitSen[i].length) {
-            longWord = splitSen[i];
-            // console.log(longWord);
+$(document).ready( function () {
+    console.log('jQuery loaded');
+    
+    longestWord = (sen) => {
+        var longWord = "";
+        var splitSen = sen.split(" ");
+    
+        for (var i = 0; i < splitSen.length; i++) {
+            if (longWord.length < splitSen[i].length) {
+                longWord = splitSen[i];
+                // console.log(longWord);
+            }
+        }
+        // code goes here
+        sen = longWord;
+        console.log(sen);
+    
+        return sen;
+    }
+    
+    isItAPalindrome = (sen) => {
+        var palindromeToCheck = sen.replace(/[.,+ ]/g, "").toLowerCase();
+        var reversedSentence = "";
+    
+        console.log(`Sen: ${sen}, ${sen.length} : palindromeToCheck: ${palindromeToCheck}`);
+    
+        for (var i = palindromeToCheck.length - 1; i >= 0; i--) {
+            console.log(`Sen at ${i} ${palindromeToCheck[i]}`);
+            reversedSentence += (palindromeToCheck[i]);
+        }
+    
+        console.log("Reversed array:", reversedSentence);
+        if (palindromeToCheck = reversedSentence) {
+            return true;
+        } else {
+            return false;
         }
     }
-    // code goes here
-    sen = longWord;
-    console.log(sen);
-
-    return sen;
-}
-
-isItAPalindrome = (sen) => {
-    var palindromeToCheck = sen.replace(/[.,+ ]/g, "").toLowerCase();
-    var reversedSentence = "";
-
-    console.log(`Sen: ${sen}, ${sen.length} : palindromeToCheck: ${palindromeToCheck}`);
-
-    for (var i = palindromeToCheck.length - 1; i >= 0; i--) {
-        console.log(`Sen at ${i} ${palindromeToCheck[i]}`);
-        reversedSentence += (palindromeToCheck[i]);
-    }
-
-    console.log("Reversed array:", reversedSentence);
-    if (palindromeToCheck = reversedSentence) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-isItAPalindrome("A car, a man, a maraca.");
-isItAPalindrome("No lemon, no melon");
-// LongestWord("This is a really positutitottallyly, verily, absolutely, positutitottallyly long sentence.");
+    
+    // isItAPalindrome("A car, a man, a maraca.");
+    // isItAPalindrome("No lemon, no melon");
+    // LongestWord("This is a really positutitottallyly, verily, absolutely, positutitottallyly long sentence.");
+});
